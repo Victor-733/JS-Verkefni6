7819ae144cbcd497f4df70eba8f490811d02d9dd
Victor-733 <victorwahid@gmail.com>
2019-04-04 17:26:42 +0000

    make images and tag array

---
+++ Verkefni6/Tags/scipt.js
@@ -0,0 +1,60 @@
+let imagesArray = [ // the images and the tags attached
+    {
+        name: "Bob Ross",
+        src: "img/bobRoss.jpg",
+        tags: ["man", "nature"]
+    },
+    {
+        name: "Boss Baby",
+        src: "img/bossBaby.jpg",
+        tags: ["child", "intelligent"]
+    },
+    {
+        name: "Brummi",
+        src: "img/brummi.jpg",
+        tags: ["veichle"]
+    },
+    {
+        name: "Car",
+        src: "img/car.jpg",
+        tags: ["veichle", "man"]
+    },
+    {
+        name: "Catgirl",
+        src: "img/catgirl.jpg",
+        tags: ["woman"]
+    },
+    {
+        name: "Corvax",
+        src: "img/corvax.jpg",
+        tags: ["man", "intelligent"]
+    },
+    {
+        name: "Polnareff",
+        src: "img/polnareff.jpg",
+        tags: ["child"]
+    },
+    {
+        name: "Trap",
+        src: "img/trap.jpg",
+        tags: ["man"]
+    }
+];
+
+const gallery = document.getElementById("images");
+const tags = document.getElementById("tags");
+
+let tagsArray = [];
+
+imagesArray.forEach(function(image) { // make the image and append it to the gallery
+    const img = new Image();
+    img.src = image.src;
+    gallery.appendChild(img);
+    // check the tags and add them to a list for a complete tag list
+    image.tags.forEach(tag){
+        if (!tagsArray.includes(tag)){
+            tagsArray.push(tag);
+        }
+    };
+});
+
